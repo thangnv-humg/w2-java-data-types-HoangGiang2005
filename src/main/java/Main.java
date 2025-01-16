a3ac6313d6122906eddebfe0d1fb85b4a678ab1a
HoangGiang2005 <giangkaka2005@gmail.com>
2025-01-16 14:20:03 +0700

    BaiTap

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: Khai báo biến và viết chương trình theo yêu cầu trong README.md
+        
+        int tuoi = 42;
+        double chieu_cao = 1.75;
+        String ten = "nguyen van a";
+
+        System.out.println(tuoi);
+        System.out.println(chieu_cao);
+        System.out.println(ten);
+
+        if(tuoi > 10){
+            System.out.println("Greater than 10");
+        }
+        else{
+            System.out.println("Less or equal 10");
+        }
 
         // Ví dụ:
         // int myInt = 42;
